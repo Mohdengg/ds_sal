8a9a508db7d295768cae0695912b09594a7cac82
Mohdengg <katpadiengg@gmail.com>
2021-02-06 08:40:40 +0530

    Just added the scrapper

---
+++ data_collection.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  6 08:36:31 2021
+
+@author: Katpadi
+"""
+
+
+import glassdoor_scrapper as gs
+import pandas as pd
+path ="C:/Users/Katpadi/Documents/DataScience/Ken Jee/sal_proj/chromedriver"
+slp_time= 15
+df = gs.get_jobs("data scientist",40, False, path, slp_time)
